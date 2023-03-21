e34e34f5aa12813188b632439629e5ef79391364
jayce01 <47369842+jayce01@users.noreply.github.com>
2023-03-21 16:35:55 -0400

    Add files via upload

---
+++ Encoder.py
@@ -0,0 +1,54 @@
+def encode(password):
+    newPass = ''
+    for x in password:
+        num = int(x)
+        if num == 9:
+            num = 2
+        elif num == 8:
+            num = 1
+        elif num == 7:
+            num = 0
+        else:
+            num = num + 3
+        newPass += str(num)
+    return newPass
+
+
+def decode(newPass):
+    oldPass = ''
+    for x in newPass:
+        num = int(x)
+        if num == 0:
+            num = 7
+        elif num == 1:
+            num = 8
+        elif num == 2:
+            num = 9
+        else:
+            num = num - 3
+        oldPass += str(num)
+    return oldPass
+
+
+def menu():
+    print("""
+    Menu
+    -----------
+    1. Encode
+    2. Decode
+    3. Quit
+    """)
+
+def main():
+    menu()
+    option = 0
+    userPass = ''
+    while option != 3:
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            userPass = input("Please enter a password to encode: ")
+            print("Your password has been encoded and stored!")
+            userPass = encode(userPass)
+        if option == 2:
+            print(f"The encoded password is {userPass} and the original password is " + decode(userPass))
+main()
